bc7176c370b0e4a30a15347d1fc4cc004fa8a95d
MattyIce <144847208+Matt-y-Ice@users.noreply.github.com>
2025-05-04 15:35:25 -0400

    Add files via upload

--- gentoo-first-boot.bash
+++ gentoo-first-boot.bash
@@ -17,22 +17,23 @@ install_sudo () {
 
 install_gnome () {
     echo "gnome-base/gnome -extras" > /etc/portage/package.use/gnome
-    echo "media-video/pipewire" > /etc/portage/package.use/pipewire
-    emerge gnome-base/gnome app-arch/file-roller gnome-base/dconf-editor gnome-extra/gnome-calculator gnome-extra/gnome-system-monitor gnome-extra/gnome-tweaks gnome-extra/gnome-weather sys-apps/gnome-disk-utility sys-apps/baobab aa-eselect/eselect-gnome-shell-extensions
-    systemctl enable --now gdm
-    emerge video-media/pipewire video-media/wireplumber
-    #systemctl --user enable --now pipewire-pulse.socket wireplumber-service
-    #systemctl --user enable --now pipewire.service
+    
+    emerge gnome-base/gnome app-arch/file-roller gnome-base/dconf-editor gnome-extra/gnome-calculator gnome-extra/gnome-system-monitor gnome-extra/gnome-tweaks gnome-extra/gnome-weather sys-apps/gnome-disk-utility sys-apps/baobab gnome-extra/gnome-shell-extensions app-eselect/eselect-gnome-shell-extensions
+  
     emerge media-libs/openal
+}
+
+install_audio () {
+    echo "media-video/pipewire pipewire-alsa sound-server systemd dbus X bluetooth" > /etc/portage/package.use/pipewire
+    echo "media-video/wireplumber systemd" /etc/portage/package.use/wireplumber
+    echo "media-libs/libcanberra gtk3" > /etc/portage/package.use/libcanberra
+    emerge media-video/pipewire media-video/wireplumber media-libs/libcanberra
 }
 
 install_flatpak () {
     emerge sys-apps/flatpak
     flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     emerge sys-apps/xdg-desktop-portal sys-apps/xdg-desktop-portal-gnome sys-apps/xdg-desktop-portal-gtk
-    echo "gnome-extra/gnome-software flatpak" > /etc/portage/package.use/gnome-software
-    echo "gnome-extra/gnome-software ~amd64" > /etc/portage/package.use/gnome-software
-    emerge gnome-extra/gnome-software
 }
 
 install_applications () {
@@ -42,16 +43,34 @@ install_applications () {
     eselect repository enable gentoo-zh
     emaint sync
     emerge www-client/thorium-browser-bin
+
+    echo "app-editors/emacs gtk -X gui inotify dbus gfile jpeg png svg webp json threads harfbuzz tree-sitter jit sound dynamic-loading" > /etc/portage/package.use/emacs
+    emerge app-editors/emacs app-emacs/vterm app-emacs/nerd-icons
+
+    echo "dev-vcs/git curl gpg iconv nls pcre perl highlight keyring" > /etc/portage/package.use/git
+    emerge dev-vcs/git dev-vcs/gitg
+    git config --global user.email "matty_ice_2011@pm.me"
+    git config --global user.name "mattyice"
+    
 }
 
 main () {
     rm /stage3-*.tar.*
        
     install_gnome
+    install_audio
     install_flatpak
     install_sudo
     create_user
     install_applications
-    
-    echo -e "${RED}!!! Reminder: Type /"visudo/" command to uncomment wheel line!${RESET}"
+
+    systemctl enable gdm
+    echo -e "${RED}!!! Reminder: Type /"visudo/" command to uncomment wheel line!\n!!! Reminder: config systemd to enable audio${RESET}"
+    # systemctl --user disable pulseaudio.service pulseaudio.socket
+    # systemctl --user disable pipewire-media-session.service
+    # systemct --user enable --now pipewire.socket pipewire-pulse.socket
+    # systemct --user enable --now wireplumber.service
+    # switch to root -- execute visudo
+
+    reboot
 }
